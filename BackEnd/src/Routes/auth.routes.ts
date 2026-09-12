6893d2b4ecf4e1a071187d506c821f3f02919fef
vitormanoeldossantossilva0 <vitormanoeldossantossilva0@gmail.com>
2026-09-12 17:19:33 -0300

    corrigindo bugs

--- BackEnd/src/Routes/auth.routes.ts
+++ BackEnd/src/Routes/auth.routes.ts
@@ -5,7 +5,7 @@ import prisma from "../lib/prisma";
 import { authMiddleware } from "../middleware/auth";
 
 const router = Router();
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET: string = process.env.JWT_SECRET || "";
 
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET não configurado no ambiente.");
--- BackEnd/src/middleware/auth.ts
+++ BackEnd/src/middleware/auth.ts
@@ -1,7 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET: string = process.env.JWT_SECRET || "";
 
 if (!JWT_SECRET) {
   throw new Error("JWT_SECRET não configurado no ambiente.");
