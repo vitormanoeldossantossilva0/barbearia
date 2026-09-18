7203506c3d79f04108715ffd6df2a7a6a9008d29
vitormanoeldossantossilva0 <vitormanoeldossantossilva0@gmail.com>
2026-09-18 15:40:27 -0300

    att

--- BackEnd/src/lib/prisma.ts
+++ BackEnd/src/lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { PrismaClient } from "../../prisma/generated/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
